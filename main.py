199d138e74cd225d3179e1459d91ef58616574c6
Eadral <a250014238@me.com>
2019-03-31 00:31:58 +0800

    update: better experience

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from test import test
 import sys
 
 if __name__ == "__main__":
-    test_data = r"C:\Develop\PyCharm\OOHelper\test_data"
-    jar = r".;C:\Study\OO\homework\unit2\elevator-input-hw1-1.3-jar-with-dependencies.jar;C:\Study\OO\homework\unit2\timable-output-1.0-raw-jar-with-dependencies.jar "
+    test_data = r"test_data"
+    jar = ";".join([
+        r".",
+        r"C:\Study\OO\homework\unit2\elevator-input-hw1-1.3-jar-with-dependencies.jar",
+        r"C:\Study\OO\homework\unit2\timable-output-1.0-raw-jar-with-dependencies.jar",
+    ])
+    timeout = 30  # 200
 
-    test(jar, test_data, r"C:\Study\OO\homework\unit2\oo_course_2019_16191051_homework_5", [], "Main", "")
+    test(timeout, jar, test_data, r"C:\Study\OO\homework\unit2\oo_course_2019_16191051_homework_5", [], "Main", "", ".")
 
 
